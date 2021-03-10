628fd8a33f3edb471a4b6767ca362e7281d863f0
mlx-kva <kva@melexis.com>
2021-03-10 15:34:42 -0800

    speed up the read communication routine

--- mcp2221/__init__.py
+++ mcp2221/__init__.py
@@ -4,4 +4,4 @@ from .mcp2221 import load_driver
 load_driver()
 
 
-__version__ = "0.1.1"
+__version__ = "0.2.1"
--- mcp2221/c-code/src/mcp2221_hidapi.c
+++ mcp2221/c-code/src/mcp2221_hidapi.c
@@ -3,8 +3,36 @@
 #include <string.h> 
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include <sys/time.h>
+
+//
+// Yet another lib for MCP2221 https://www.microchip.com/wwwproducts/en/MCP2221A
+//
+// Inspired by:
+// - https://github.com/nonNoise/PyMCP2221A
+// - https://github.com/zkemble/libmcp2221
+// - https://elixir.bootlin.com/linux/latest/source/drivers/hid/hid-mcp2221.c
+//
+
+
+/* Response codes in a raw input report */
+enum {
+  MCP2221_SUCCESS = 0x00,
+  MCP2221_I2C_ENG_BUSY = 0x01,
+  MCP2221_I2C_START_TOUT = 0x12,
+  MCP2221_I2C_STOP_TOUT = 0x62,
+  MCP2221_I2C_WRADDRL_TOUT = 0x23,
+  MCP2221_I2C_WRDATA_TOUT = 0x44,
+  MCP2221_I2C_WRADDRL_NACK = 0x25,
+  MCP2221_I2C_MASK_ADDR_NACK = 0x40,
+  MCP2221_I2C_WRADDRL_SEND = 0x21,
+  MCP2221_I2C_ADDR_NACK = 0x25,
+  MCP2221_I2C_READ_COMPL = 0x55,
+  MCP2221_ALT_F_NOT_GPIOV = 0xEE,
+  MCP2221_ALT_F_NOT_GPIOD = 0xEF,
+};
 
 
 static uint64_t
@@ -228,6 +256,36 @@ mcp2221_hidapi_i2c_test_lines(struct MCP2221_t *handle)
 }
 
 
+static int mcp_get_i2c_engine_state(struct MCP2221_t *handle, uint8_t idx)
+{
+  int ret;
+
+  switch (handle->in_report_[idx])
+  {
+    case MCP2221_I2C_WRADDRL_NACK:
+    case MCP2221_I2C_WRADDRL_SEND:
+      ret = -ENXIO;
+      break;
+    case MCP2221_I2C_START_TOUT:
+    case MCP2221_I2C_STOP_TOUT:
+    case MCP2221_I2C_WRADDRL_TOUT:
+    case MCP2221_I2C_WRDATA_TOUT:
+      ret = -ETIMEDOUT;
+      break;
+    case MCP2221_I2C_ENG_BUSY:
+      ret = -EAGAIN;
+      break;
+    case MCP2221_SUCCESS:
+      ret = 0x00;
+      break;
+    default:
+      ret = -EIO;
+  }
+
+  return ret;
+}
+
+
 int16_t
 mcp2221_hidapi_i2c_set_frequency(struct MCP2221_t *handle, uint32_t frequency_hz)
 {
@@ -235,6 +293,10 @@ mcp2221_hidapi_i2c_set_frequency(struct MCP2221_t *handle, uint32_t frequency_hz
   {
     printf("%s: ERROR: No handle\n", __func__);
     return -9999;
+  }
+  if (frequency_hz == 0) // use 0 to resend last used frequency!
+  {
+    frequency_hz = handle->i2c_frequency_hz_;
   }
   mcp2221_hidapi_clear_reports(handle);
   handle->out_report_[0] = 0x10; // CMD Status / Set param
@@ -244,6 +306,13 @@ mcp2221_hidapi_i2c_set_frequency(struct MCP2221_t *handle, uint32_t frequency_hz
   handle->out_report_[4] = (12000000 / frequency_hz) - 3; // clock speed.
   mcp2221_hidapi_sent_report(handle);
   mcp2221_hidapi_receive_report(handle);
+
+  uint8_t state = mcp_get_i2c_engine_state(handle, 8);
+  if (state != MCP2221_SUCCESS)
+  {
+    usleep(1000);    /* Small delay is needed here */
+    mcp2221_hidapi_i2c_cancel(handle);
+  }
 
   handle->i2c_frequency_hz_ = frequency_hz;
 
@@ -264,14 +333,11 @@ mcp2221_hidapi_i2c_cancel(struct MCP2221_t *handle)
   mcp2221_hidapi_sent_report(handle);
   mcp2221_hidapi_receive_report(handle);
 
-  if (handle->in_report_[8] != 0)
+  if (handle->in_report_[8] != MCP2221_SUCCESS)
   {
     mcp2221_hidapi_clear_reports(handle);
     handle->out_report_[0] = 0x10; // CMD Status / Set param
-    handle->out_report_[1] = 0x00; // don't care
     handle->out_report_[2] = 0x10; // 0x10 ==> cancel current transaction
-    handle->out_report_[3] = 0x20; // when 0x20 ==> next is clock divider
-    handle->out_report_[4] = (12000000 / handle->i2c_frequency_hz_) - 3; // clock speed.
     mcp2221_hidapi_sent_report(handle);
     return mcp2221_hidapi_receive_report(handle);
   }
@@ -374,6 +440,7 @@ mcp2221_hidapi_i2c_read_(struct MCP2221_t *handle, uint8_t cmd, uint8_t slave_ad
     printf("%s: ERROR: No handle\n", __func__);
     return -9999;
   }
+
   mcp2221_hidapi_clear_reports(handle);
   handle->out_report_[0] = cmd;
   handle->out_report_[1] = size & 0x00FF;
@@ -395,16 +462,15 @@ mcp2221_hidapi_i2c_read_(struct MCP2221_t *handle, uint8_t cmd, uint8_t slave_ad
     if (chunk_size > 60) chunk_size = 60;
     if (chunk_size <= 0) break;
 
-    uint64_t t_start = get_time_in_us();
-    while ((get_time_in_us() - t_start) < 100000)
-    {
-      if (mcp2221_hidapi_i2c_state_check(handle) == 0x55)
-      { // data is available!
-        break;
-      }
-    }
-
-
+    usleep(1100);
+    // uint64_t t_start = get_time_in_us();
+    // while ((get_time_in_us() - t_start) < 100000)
+    // {
+    //   if (mcp2221_hidapi_i2c_state_check(handle) == 0x55)
+    //   { // data is available!
+    //     break;
+    //   }
+    // }
 
     mcp2221_hidapi_clear_reports(handle);
     handle->out_report_[0] = 0x40;
@@ -426,6 +492,10 @@ mcp2221_hidapi_i2c_read_(struct MCP2221_t *handle, uint8_t cmd, uint8_t slave_ad
     for (int i=0; i<chunk_size; i++)
     {
       data[(chunk_i * 60) + i] = handle->in_report_[i+4];
+    }
+    if (mcp2221_hidapi_i2c_state_check(handle) == 0x00)
+    {
+      return 0;
     }
   }
   return 0;
@@ -482,6 +552,7 @@ mcp2221_hidapi_i2c_memory_write(struct MCP2221_t *handle, uint8_t slave_address,
   buf[2] = (data >> 8) & 0xFF;
   buf[3] = data & 0xFF;
 
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   int16_t r = mcp2221_hidapi_i2c_write(handle, slave_address, buf, 4);
   if (delay_ms > 0)
   {
@@ -499,6 +570,7 @@ mcp2221_hidapi_i2c_memory_read(struct MCP2221_t *handle, uint8_t slave_address, 
   buf[0] = (memory_address >> 8) & 0xFF;
   buf[1] = memory_address & 0xFF;
 
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   int16_t r = mcp2221_hidapi_i2c_write_no_stop(handle, slave_address, buf, 2);
   if (r != 0)
   {
@@ -513,6 +585,8 @@ int16_t
 mcp2221_hidapi_i2c_memory_read_uint16(struct MCP2221_t *handle, uint8_t slave_address, uint16_t memory_address, uint16_t *data, uint16_t size)
 {
   uint8_t *data8 = (uint8_t *)data;
+
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   int16_t r = mcp2221_hidapi_i2c_memory_read(handle, slave_address, memory_address, data8, size*2);
   for (uint16_t i=0; i<size; i++)
   { // swap bytes: 
@@ -527,6 +601,8 @@ mcp2221_hidapi_i2c_write_byte(struct MCP2221_t *handle, uint8_t slave_address, u
 {
   uint8_t buf[1];
   buf[0] = data;
+
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   return mcp2221_hidapi_i2c_write(handle, slave_address, buf, 1);
 }
 
@@ -537,6 +613,8 @@ mcp2221_hidapi_i2c_write_word(struct MCP2221_t *handle, uint8_t slave_address, u
   uint8_t buf[2];
   buf[0] = data & 0xFF;
   buf[1] = (data >> 8) & 0xFF;
+
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   return mcp2221_hidapi_i2c_write(handle, slave_address, buf, 2);
 }
 
@@ -544,6 +622,7 @@ mcp2221_hidapi_i2c_write_word(struct MCP2221_t *handle, uint8_t slave_address, u
 int16_t
 mcp2221_hidapi_i2c_read_byte(struct MCP2221_t *handle, uint8_t slave_address, uint8_t *data)
 {
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   return mcp2221_hidapi_i2c_read(handle, slave_address, data, 1);
 }
 
@@ -552,6 +631,8 @@ int16_t
 mcp2221_hidapi_i2c_read_word(struct MCP2221_t *handle, uint8_t slave_address, uint16_t *data)
 {
   uint8_t *data8 = (uint8_t *)data;
+
+  mcp2221_hidapi_i2c_set_frequency(handle, 0);
   int16_t r = mcp2221_hidapi_i2c_read(handle, slave_address, data8, 2);
 
   // swap bytes: 
